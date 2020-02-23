29ca816fcf67946701ae189d42ad270d5c362b2f
Derek <derekkarmolinski@gmail.com>
2020-02-22 19:37:33 -0500

    Screen Shake and particles

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,11 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("w"):
+		$Camera.add_trauma(5)
+	if Input.is_action_pressed("a"):
+		$Camera.add_trauma(50)
+	if Input.is_action_pressed("s"):
+		$Camera.add_trauma(100)
+	if Input.is_action_pressed("d"):
+		$Camera.add_trauma(200)
